285ff6cc5abc69d903107644f27c63f42d293e4d
Bodo Kaiser <i@bodokaiser.io>
2019-01-07 14:35:15 +0100

    completed refactor

--- typescript/experiment-simple.ts
+++ typescript/experiment-simple.ts
@@ -6,8 +6,8 @@ import {
   Color3,
   Color4,
   MeshBuilder,
+  SolidParticle,
   SolidParticleSystem,
-  StandardMaterial,
   VertexBuffer,
   VertexData,
 } from 'babylonjs'
@@ -49,15 +49,20 @@ window.addEventListener('load', () => {
       scene.clearColor = Color3.FromHexString(Color.Background).toColor4()
 
       let laser = createLaser('laser', scene)
+      laser.position.x = -0.45
+      laser.position.y = 0.05
+      laser.isVisible = false
 
       let mirror1 = createMirror('mirror1', scene)
       mirror1.position.x = -0.39
       mirror1.position.y = 0.05
-      let mirror2 = mirror1.clone()
+      mirror1.isVisible = false
+
+      let mirror2 = mirror1.clone('mirror2')
       mirror2.position.x = 0.5
 
       let photons = createPhotons('photons', scene)
-      photons.updateParticle = particle => {
+      photons.updateParticle = (particle: SolidParticle): SolidParticle => {
         particle.position.x = uniform(mirror2.position.x, mirror1.position.x)
         particle.position.y = uniform(0.03, 0.07)
         particle.position.z = uniform(-0.025, 0.025)
@@ -72,33 +77,16 @@ window.addEventListener('load', () => {
         color[3] = I
 
         particle.color = Color4.FromArray(color)
+
+        return particle
       }
       photons.buildMesh()
       photons.mesh.hasVertexAlpha = true
 
-      let ground1 = MeshBuilder.CreateGround('ground1',
-        {
-          width: mirror2.position.x - mirror1.position.x,
-          height: 0.1, subdivisions: 500
-        }, scene)
-      ground1.material = scene.getMaterialByID('standard')
-      ground1.position.x = 0.055
-      ground1.position.y = 0.05
-      ground1.isVisible = false
-
-      let positions1 = ground1.getVerticesData(VertexBuffer.PositionKind)
-      let indices1 = ground1.getIndices()
-
-      for (let i = 0; i < positions1.length; i += 3) {
-        positions1[i + 1] = 0.01 * Math.cos(50 * Math.PI * positions1[i]) ** 2
-      }
-
-      ground1.updateVerticesData(VertexBuffer.PositionKind,
-        positions1, false, true)
-
-      let normals = []
-      VertexData.ComputeNormals(positions1, indices1, normals)
-      ground1.updateVerticesData(BABYLON.VertexBuffer.NormalKind, normals, false, false)
+      let potential1 = createPotential('potential1', scene)
+      potential1.position.x = 0.055
+      potential1.position.y = 0.05
+      potential1.isVisible = false
 
       scene.actionManager = new ActionManager(scene)
       scene.actionManager.registerAction(
@@ -122,7 +110,7 @@ window.addEventListener('load', () => {
             break
           case 4:
             photons.mesh.isVisible = false
-            ground1.isVisible = true
+            potential1.isVisible = true
             break
         }
       })
@@ -139,9 +127,6 @@ function createLaser(name: string, scene: Scene) {
     { height: 0.1, diameter: 0.05 }, scene)
   laser.material = scene.getMaterialByID('metal')
   laser.rotation.z = Math.PI / 2
-  laser.position.x = -0.45
-  laser.position.y = 0.05
-  laser.isVisible = false
 
   return laser
 }
@@ -151,7 +136,6 @@ function createMirror(name: string, scene: Scene) {
     { size: 0.1, sideOrientation: Mesh.DOUBLESIDE }, scene)
   mirror.material = scene.getMaterialByID('glass')
   mirror.rotation.y = Math.PI / 2
-  mirror.isVisible = false
 
   return mirror
 }
@@ -168,4 +152,28 @@ function createPhotons(name: string, scene: Scene) {
   disc.dispose()
 
   return photons
+}
+
+function createPotential(name: string, scene: Scene) {
+  let x1 = scene.getMeshByName('mirror1').position.x
+  let x2 = scene.getMeshByName('mirror2').position.x
+
+  let ground = MeshBuilder.CreateGround(name,
+    { width: x2 - x1, height: 0.05, subdivisions: 500 }, scene)
+  ground.material = scene.getMaterialByID('standard')
+
+  let positions = ground.getVerticesData(VertexBuffer.PositionKind)
+  let indices = ground.getIndices()
+  let normals = []
+
+  for (let i = 0; i < positions.length; i += 3) {
+    positions[i + 1] = 0.01 * Math.cos(50 * Math.PI * positions[i]) ** 2
+  }
+
+  VertexData.ComputeNormals(positions, indices, normals)
+
+  ground.updateVerticesData(VertexBuffer.PositionKind, positions, false, true)
+  ground.updateVerticesData(VertexBuffer.NormalKind, normals, false, false)
+
+  return ground
 }
