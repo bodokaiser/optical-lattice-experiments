5452cc558cef95dce99c9358910555c0a63d8776
Bodo Kaiser <i@bodokaiser.io>
2019-01-10 21:15:35 +0100

    completed lattice potential in optical-potentials

--- typescript/optical-potentials.ts
+++ typescript/optical-potentials.ts
@@ -24,9 +24,9 @@ import * as Material from './material'
 import * as Geometry from './geometry'
 import { GaussianBeam } from './physic/optic'
 
-let gaussian = new GaussianBeam(0.02, 4e-3)
-
 let state = 0
+
+let gaussian = new GaussianBeam(150e-4, 30e-4)
 
 window.addEventListener('load', () => {
   let canvas = document.getElementById('canvas') as HTMLCanvasElement
@@ -48,7 +48,7 @@ window.addEventListener('load', () => {
       light.shadowMaxZ = 10
 
       let camera = new ArcRotateCamera('camera', 0, 0, 1, Vector3.Zero(), scene)
-      camera.minZ = 0.01
+      camera.minZ = 0.001
       camera.inertia = 0.1
       camera.upperRadiusLimit = 2
       camera.lowerRadiusLimit = 0.1
@@ -57,51 +57,62 @@ window.addEventListener('load', () => {
       camera.attachControl(canvas, true)
 
       let laser1 = createLaser('laser1', scene)
-      laser1.position.x = -0.45
+      laser1.position.x = -0.6
       laser1.position.y = 0.1
-      laser1.isVisible = true
+      laser1.isVisible = false
 
-      let beam1 = createBeamDiscs('beam1', scene)
-      //beam1.position.y = 0.1
+      let beam1a = createBeam('beam1a', scene, false)
+      beam1a.rotation.y = Math.PI / 2
+      beam1a.position.y = 0.1
+      beam1a.isVisible = false
 
-      let mirror1 = createMirror('mirror1', scene)
-      mirror1.position.x = -0.39
-      mirror1.position.y = 0.05
-      mirror1.isVisible = false
+      let beam1b = createBeam('beam1b', scene, true)
+      beam1b.rotation.y = Math.PI / 2
+      beam1b.position.y = 0.1
+      beam1b.isVisible = false
 
-      let mirror2 = mirror1.clone('mirror2')
-      mirror2.position.x = 0.5
+      let mirror1a = createMirror('mirror1a', scene)
+      mirror1a.position.x = 0.5
+      mirror1a.material = laser1.material
+      mirror1a.position.y = 0.1
+      mirror1a.isVisible = false
 
-      let potential1 = createPotential('potential1', scene)
-      potential1.position.x = 0.055
-      potential1.position.y = 0.05
-      potential1.isVisible = false
+      let mirror1b = mirror1a.clone('mirror1b')
+      mirror1b.material = scene.getMaterialByName('glass')
+      mirror1b.position.x = -0.5
 
       let laser2 = laser1.clone('laser2')
       laser2.rotation.y = Math.PI / 2
       laser2.position.x = 0
-      laser2.position.z = -0.45
+      laser2.position.z = -0.6
+      laser2.isVisible = false
 
-      let mirror3 = mirror1.clone('mirror3')
-      mirror3.rotation.y = 0
-      mirror3.position.x = 0
-      mirror3.position.z = 0.39
+      let beam2b = beam1b.clone('beam2b')
+      beam2b.rotation.y = 0
 
-      let mirror4 = mirror3.clone('mirror4')
-      mirror4.position.z = -0.5
+      let mirror2a = mirror1b.clone('mirror2a')
+      mirror2a.material = scene.getMaterialByName('glass')
+      mirror2a.rotation.y = 0
+      mirror2a.position.x = 0
+      mirror2a.position.z = 0.5
 
-      let potential2 = potential1.clone('potential2')
-      potential2.rotation.y = Math.PI / 2
-      potential2.position.x = 0
-      potential2.position.z = -0.055
+      let mirror2b = mirror2a.clone('mirror2b')
+      mirror2b.position.z = -0.5
 
       scene.actionManager = new ActionManager(scene)
       scene.actionManager.registerAction(
         new ExecuteCodeAction(ActionManager.OnKeyDownTrigger, e => {
           if (e.sourceEvent.type != 'keydown') return
-          if (e.sourceEvent.key != 'x') return
 
-          console.log('state', state++)
+          switch (e.sourceEvent.key) {
+            case 'x':
+              state++
+              break
+            case 'y':
+              state--
+          }
+
+          console.log(state)
         }))
 
       scene.registerBeforeRender(() => {
@@ -109,18 +120,23 @@ window.addEventListener('load', () => {
           case 1:
             laser1.isVisible = true
             break
+          case 2:
+            mirror1a.isVisible = true
+            break
           case 3:
-            mirror1.isVisible = true
-            mirror2.isVisible = true
+            beam1a.isVisible = true
             break
           case 4:
-            potential1.isVisible = true
+            mirror1a.material = scene.getMaterialByName('glass')
+            mirror1b.isVisible = true
+            beam1a.isVisible = false
+            beam1b.isVisible = true
             break
           case 5:
             laser2.isVisible = true
-            mirror3.isVisible = true
-            mirror4.isVisible = true
-            potential2.isVisible = true
+            mirror2a.isVisible = true
+            mirror2b.isVisible = true
+            beam2b.isVisible = true
             break
         }
       })
@@ -132,45 +148,33 @@ window.addEventListener('load', () => {
 
 if (module.hot) module.hot.accept(() => location.reload())
 
-function createBeamTube(name: string, scene: Scene) {
+function createBeam(name: string, scene: Scene, standing = false) {
   let curve = []
-  for (let i = 0; i < 100; i++) {
-    curve.push(new Vector3(0, 0, i / 100 - 0.5))
+  for (let i = 0; i < 1000; i++) {
+    curve.push(new Vector3(0, 0, i / 1000 - 0.5))
   }
 
-  let radius = (index: number, distance: number): number => {
-    return gaussian.waist(distance - 0.5)
+  let radius = (_, distance: number): number => {
+    let z = distance - 0.5
+    let r = gaussian.waist(z)
+
+    if (standing) {
+      r *= Math.cos(100 * Math.PI * z) ** 2
+    }
+
+    return r
   }
 
-  let beam = MeshBuilder.CreateTube('beam1',
+  let beam = MeshBuilder.CreateTube(name,
     { path: curve, radiusFunction: radius, cap: Mesh.CAP_ALL }, scene)
   beam.material = scene.getMaterialByID('standard')
 
   return beam
 }
 
-function createBeamDiscs(name: string, scene: Scene) {
-  let discs = []
-
-  for (let i = 0; i < 100; i++) {
-    let d = 1e-2 * i - 0.5
-
-    let disc = MeshBuilder.CreateCylinder(`${name}${i}`,
-      { diameter: gaussian.waist(d), height: 1e-2 }, scene)
-    disc.material = scene.getMaterialByID('standard')
-    disc.position.y = 0.1
-    disc.position.x = d
-    disc.rotation.z = Math.PI / 2
-
-    discs.push(disc)
-  }
-
-  return discs
-}
-
 function createLaser(name: string, scene: Scene) {
   let laser = MeshBuilder.CreateCylinder(name,
-    { height: 0.15, diameter: 0.1 }, scene)
+    { height: 0.15, diameter: 0.12, tessellation: 40 }, scene)
   laser.material = scene.getMaterialByID('metal')
   laser.rotation.z = Math.PI / 2
 
@@ -179,33 +183,9 @@ function createLaser(name: string, scene: Scene) {
 
 function createMirror(name: string, scene: Scene) {
   let mirror = MeshBuilder.CreatePlane(name,
-    { size: 0.1, sideOrientation: Mesh.DOUBLESIDE }, scene)
+    { size: 0.2, sideOrientation: Mesh.DOUBLESIDE }, scene)
   mirror.material = scene.getMaterialByID('glass')
   mirror.rotation.y = Math.PI / 2
 
   return mirror
-}
-
-function createPotential(name: string, scene: Scene) {
-  let x1 = scene.getMeshByName('mirror1').position.x
-  let x2 = scene.getMeshByName('mirror2').position.x
-
-  let ground = MeshBuilder.CreateGround(name,
-    { width: x2 - x1, height: 0.05, subdivisions: 500 }, scene)
-  ground.material = scene.getMaterialByID('standard')
-
-  let positions = ground.getVerticesData(VertexBuffer.PositionKind)
-  let indices = ground.getIndices()
-  let normals = []
-
-  for (let i = 0; i < positions.length; i += 3) {
-    positions[i + 1] = 0.01 * Math.cos(50 * Math.PI * positions[i]) ** 2
-  }
-
-  VertexData.ComputeNormals(positions, indices, normals)
-
-  ground.updateVerticesData(VertexBuffer.PositionKind, positions, false, true)
-  ground.updateVerticesData(VertexBuffer.NormalKind, normals, false, false)
-
-  return ground
 }
--- typescript/physic/optic.ts
+++ typescript/physic/optic.ts
@@ -1,24 +1,30 @@
 export class GaussianBeam {
 
-  private _waist0  : number
-  private _rlength : number
+  private _waist0: number
+  private _lambda: number
+  private _rlength: number
 
-  constructor(waist0 : number, lambda : number) {
+  constructor(waist0: number, lambda: number) {
     this._waist0 = waist0
-    this._rlength = Math.PI * waist0**2 / lambda
+    this._lambda = lambda
+    this._rlength = Math.PI * waist0 ** 2 / lambda
   }
 
-  waist(z : number) : number {
+  lambda(): number {
+    return this._lambda
+  }
+
+  waist(z: number): number {
     if (z == 0) return this._waist0
 
-    return this._waist0 * Math.sqrt(1 + (z/this._rlength)**2)
+    return this._waist0 * Math.sqrt(1 + (z / this._rlength) ** 2)
   }
 
-  intensity(r : number, z : number) : number {
+  intensity(r: number, z: number): number {
     let w0 = this.waist(0)
     let wz = this.waist(z)
 
-    return (w0 / wz)**2 * Math.exp(-2 * (r / wz)**2)
+    return (w0 / wz) ** 2 * Math.exp(-2 * (r / wz) ** 2)
   }
 
 }
