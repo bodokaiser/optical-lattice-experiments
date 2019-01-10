513e0a1e2407e81ac593125700d56f1e4b5e0c25
Bodo Kaiser <i@bodokaiser.io>
2019-01-10 22:53:36 +0100

    completed optical-potentials

--- typescript/optical-potentials.ts
+++ typescript/optical-potentials.ts
@@ -1,13 +1,16 @@
 import {
   ActionManager,
+  Animation,
   ArcRotateCamera,
-  ExecuteCodeAction,
-  Mesh,
-  Scene,
   Color3,
   Color4,
   DirectionalLight,
+  ExecuteCodeAction,
+  Mesh,
   MeshBuilder,
+  Scene,
+  SolidParticleSystem,
+  StandardMaterial,
   Vector3,
   VertexBuffer,
   VertexData,
@@ -22,9 +25,11 @@ import {
 } from './builder'
 import * as Material from './material'
 import * as Geometry from './geometry'
+import { uniform } from './numeric'
 import { GaussianBeam } from './physic/optic'
 
-let state = 0
+let time = 0
+let state = 6
 
 let gaussian = new GaussianBeam(150e-4, 30e-4)
 
@@ -55,6 +60,8 @@ window.addEventListener('load', () => {
       camera.upperBetaLimit = 0.9 * Math.PI / 2
       camera.setPosition(new Vector3(1, 1, 1))
       camera.attachControl(canvas, true)
+
+      let grid = scene.getMeshByName('grid')
 
       let laser1 = createLaser('laser1', scene)
       laser1.position.x = -0.6
@@ -94,6 +101,37 @@ window.addEventListener('load', () => {
       let mirror2b = mirror2a.clone('mirror2b')
       mirror2b.position.z = -0.5
 
+      let potential = createPotential('potential', scene)
+
+      let atoms = createAtoms('atoms', scene)
+
+      let box = createBoxPotential('box', scene)
+      box.position.y = 0.05
+      box.rotation.y = Math.PI / 2
+
+      let barrier = createBarrierPotential('barrier', scene)
+      barrier.position.y = 0.05
+
+      let step = 0.1
+
+      let animation = new Animation('animation', 'position',
+        10, Animation.ANIMATIONTYPE_VECTOR3, Animation.ANIMATIONLOOPMODE_CYCLE)
+      animation.setKeys([
+        { frame: 0, value: new Vector3(step, 0.05, step) },
+        { frame: 1, value: new Vector3(0.0, 0.05, step) },
+        { frame: 2, value: new Vector3(-step, 0.05, step) },
+        { frame: 3, value: new Vector3(step, 0.05, 0.0) },
+        { frame: 4, value: new Vector3(0.0, 0.05, 0.0) },
+        { frame: 5, value: new Vector3(-step, 0.05, 0.0) },
+        { frame: 6, value: new Vector3(step, 0.05, -step) },
+        { frame: 7, value: new Vector3(0.0, 0.05, -step) },
+        { frame: 8, value: new Vector3(step, 0.05, -step) }
+      ])
+
+      let perturbation = createPerturbationPotential('perturbation', scene)
+      perturbation.position.y = 0.05
+      perturbation.animations.push(animation)
+
       scene.actionManager = new ActionManager(scene)
       scene.actionManager.registerAction(
         new ExecuteCodeAction(ActionManager.OnKeyDownTrigger, e => {
@@ -111,8 +149,11 @@ window.addEventListener('load', () => {
         }))
 
       scene.registerBeforeRender(() => {
+        time += scene.getEngine().getDeltaTime()
+
         switch (state) {
           case 0:
+            grid.isVisible = true
             beam1a.isVisible = false
             beam1b.isVisible = false
             beam2b.isVisible = false
@@ -122,6 +163,7 @@ window.addEventListener('load', () => {
             mirror1b.isVisible = false
             mirror2a.isVisible = false
             mirror2b.isVisible = false
+            potential.isVisible = false
             break
           case 1:
             mirror1a.isVisible = false
@@ -159,6 +201,11 @@ window.addEventListener('load', () => {
             mirror2b.isVisible = true
             break
           case 6:
+            box.isVisible = false
+            barrier.isVisible = false
+            grid.isVisible = true
+            atoms.isVisible = false
+            beam1a.isVisible = false
             beam1b.isVisible = false
             beam2b.isVisible = false
             laser1.isVisible = false
@@ -167,6 +214,37 @@ window.addEventListener('load', () => {
             mirror1b.isVisible = false
             mirror2a.isVisible = false
             mirror2b.isVisible = false
+            potential.isVisible = false
+            perturbation.isVisible = false
+            break
+          case 7:
+            grid.isVisible = false
+            atoms.isVisible = false
+            potential.isVisible = true
+            break
+          case 8:
+            atoms.isVisible = true
+            break
+          case 9:
+            box.isVisible = false
+            atoms.isVisible = false
+            break
+          case 10:
+            box.isVisible = true
+            break
+          case 11:
+            box.isVisible = true
+            box.position.z = 0.3 * Math.cos(0.001 * time)
+            break
+          case 12:
+            box.isVisible = false
+            barrier.isVisible = true
+            perturbation.isVisible = false
+            break
+          case 13:
+            barrier.isVisible = false
+            perturbation.isVisible = true
+            scene.beginAnimation(perturbation, 0, 100, true)
             break
         }
       })
@@ -177,6 +255,7 @@ window.addEventListener('load', () => {
 })
 
 if (module.hot) module.hot.accept(() => location.reload())
+
 
 function createBeam(name: string, scene: Scene, standing = false) {
   let curve = []
@@ -202,6 +281,7 @@ function createBeam(name: string, scene: Scene, standing = false) {
   return beam
 }
 
+
 function createLaser(name: string, scene: Scene) {
   let laser = MeshBuilder.CreateCylinder(name,
     { height: 0.15, diameter: 0.12, tessellation: 40 }, scene)
@@ -211,6 +291,7 @@ function createLaser(name: string, scene: Scene) {
   return laser
 }
 
+
 function createMirror(name: string, scene: Scene) {
   let mirror = MeshBuilder.CreatePlane(name,
     { size: 0.2, sideOrientation: Mesh.DOUBLESIDE }, scene)
@@ -218,4 +299,92 @@ function createMirror(name: string, scene: Scene) {
   mirror.rotation.y = Math.PI / 2
 
   return mirror
+}
+
+function createAtom(name: string, scene: Scene) {
+  let material = scene.getMaterialByName('metal') as StandardMaterial
+  material.diffuseColor = new Color3(0.3, 0.3, 0.3)
+  material.specularColor = new Color3(0.05, 0.05, 0.05)
+
+  let sphere = MeshBuilder.CreateSphere(name, { diameter: 0.03 }, scene)
+  sphere.material = material
+
+  return sphere
+}
+
+
+function createAtoms(name: string, scene: Scene) {
+  let atoms = new SolidParticleSystem(name, scene)
+  let atom = createAtom('atom', scene)
+
+  atoms.addShape(atom, 50)
+  atom.dispose()
+  atoms.computeParticleRotation = false
+  atoms.computeParticleTexture = false
+  atoms.computeParticleColor = false
+  atoms.updateParticle = (atom) => {
+    atom.position.x = Math.round(uniform(-4, +4)) / 10
+    atom.position.z = Math.round(uniform(-4, +4)) / 10
+  }
+  atoms.buildMesh()
+  atoms.setParticles()
+
+  return atoms.mesh
+}
+
+
+function createPotential(name: string, scene: Scene) {
+  let material = scene.getMaterialByName('standard') as StandardMaterial
+  material.diffuseColor = Color3.FromHexString(Color.Glass)
+  material.specularColor = new Color3(0.05, 0.05, 0.05)
+
+  let ground = MeshBuilder.CreateGround(name,
+    { width: 0.9, height: 0.9, subdivisions: 500, updatable: true }, scene)
+  ground.material = material
+
+  let positions = ground.getVerticesData(VertexBuffer.PositionKind)
+  let indices = ground.getIndices()
+  let normals = []
+
+  for (let i = 0; i < positions.length; i += 3) {
+    let k = 10 * Math.PI
+    let x = positions[i] - 0.5
+    let z = positions[i + 2] - 0.5
+
+    positions[i + 1] += -0.1 * Math.cos(k * x) ** 2 * Math.cos(k * z) ** 2
+  }
+
+  VertexData.ComputeNormals(positions, indices, normals)
+
+  ground.updateVerticesData(VertexBuffer.PositionKind, positions, false, true)
+  ground.updateVerticesData(VertexBuffer.NormalKind, normals, false, false)
+
+  return ground
+}
+
+
+function createBoxPotential(name: string, scene: Scene) {
+  let box = MeshBuilder.CreateBox(name,
+    { width: 0.05, height: 0.2, depth: 0.5 }, scene)
+  box.material = scene.getMaterialByName('standard')
+
+  return box
+}
+
+
+function createBarrierPotential(name: string, scene: Scene) {
+  let barrier = MeshBuilder.CreateBox(name,
+    { width: 0.3, height: 0.2, depth: 0.3 }, scene)
+  barrier.material = scene.getMaterialByName('standard')
+
+  return barrier
+}
+
+
+function createPerturbationPotential(name: string, scene: Scene) {
+  let perturbation = MeshBuilder.CreateCylinder(name,
+    { diameter: 0.1, height: 0.3 }, scene)
+  perturbation.material = scene.getMaterialByName('standard')
+
+  return perturbation
 }
